486173bf64fb591224145ae4765d9143bda82c8a
Kuwa Lee <kuwalee1069@gmail.com>
2024-11-16 01:49:34 +0800

    update

--- extract and clean.sh
+++
@@ -1,3 +0,0 @@
-cd /home/kuwalee/repo/bluesky-social-app/
-yarn intl:extract --locale zh-TW --clean
-ln ~/repo/bluesky-social-app/src/locale/locales/zh-TW/messages.po /home/kuwalee/repo/bsky-zh/zh-TW/messages.po
